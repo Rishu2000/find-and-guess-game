7c3fa7cc7f7327c1922a99e0eb7ac8ef532c5c32
Your Name <you@example.com>
2021-02-10 12:35:07 -0600

    Created State object and points.

--- src/components/App.js
+++ src/components/App.js
@@ -4,29 +4,38 @@ import React, { Component } from 'react'
 import {clickedCoords} from "../helper/coords"
 
 export default class App extends Component {
+  state = {
+    pick1 : false,
+    pick2 : false,
+    pick3 : false,
+    point:0
+  }
   handleClick = (e) => {
     e.preventDefault();
     const {pageX,pageY} = e;
     console.log({pageX,pageY});
     //For Picture 1
-    if(clickedCoords(pageX,pageY,[150,132,208,205])){
+    if(!this.state.pick1 && clickedCoords(pageX,pageY,[150,132,208,205])){
       if(window.prompt("What Do you see?") === "picture 1"){
+        this.setState({pick1: true, point:this.state.point + 1});
         window.alert("Wow You won!");
       }else{
         window.alert("Try Again!");
       }
     }
     //For Picture 2
-    if(clickedCoords(pageX,pageY,[242,94,257,205])){
+    if(!this.state.pick2 && clickedCoords(pageX,pageY,[242,94,257,205])){
       if(window.prompt("What Do you see?") === "picture 2"){
+        this.setState({pick2: true, point:this.state.point + 1});
         window.alert("Wow You won!");
       }else{
         window.alert("Try Again!");
       }
     }
     //For Picture 3
-    if(clickedCoords(pageX,pageY,[361,87,436,225])){
+    if(!this.state.pick3 && clickedCoords(pageX,pageY,[361,87,436,225])){
       if(window.prompt("What Do you see?") === "picture 3"){
+        this.setState({pick3: true, point:this.state.point + 1});
         window.alert("Wow You won!");
       }else{
         window.alert("Try Again!");
