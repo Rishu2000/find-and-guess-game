bcbbd85409903a37d99881f7db3de8a536ee765d
Your Name <you@example.com>
2021-02-10 08:01:07 -0600

    Added background image according to size.

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react'
 export default class App extends Component {
   render() {
     return (
-      <div className="bg-image">
+      <div className="App">
         Rishav
       </div>
     )
