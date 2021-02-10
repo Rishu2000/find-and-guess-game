7be46cb95717430964bc4630ec965e15e7f1568c
Your Name <you@example.com>
2021-02-10 11:58:12 -0600

    For every picture created codes.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import "../styles/App.css";
 
 import React, { Component } from 'react'
+import {clickedCoords} from "../helper/coords"
 
 export default class App extends Component {
   handleClick = (e) => {
     e.preventDefault();
     const {pageX,pageY} = e;
-    console.log(pageX,pageY);
+    console.log({pageX,pageY});
+    //For Picture 1
+    console.log("Pic 1 ="+clickedCoords(pageX,pageY,[150,132,208,205]));
+    //For Picture 2
+    console.log("Pic 2 ="+clickedCoords(pageX,pageY,[242,94,257,205]));
+    //For Picture 3
+    console.log("Pic 3 ="+clickedCoords(pageX,pageY,[361,87,436,225]));
   }
   render() {
     return (
