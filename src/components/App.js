807dceb4b67e51622ec5909ada345655e94edaa2
Your Name <you@example.com>
2021-02-10 12:12:14 -0600

    Added "window." because we are cooding in React.

--- src/components/App.js
+++ src/components/App.js
@@ -10,26 +10,26 @@ export default class App extends Component {
     console.log({pageX,pageY});
     //For Picture 1
     if(clickedCoords(pageX,pageY,[150,132,208,205])){
-      if(prompt("What Do you see?") === "picture 1"){
-        alert("Wow You won!");
+      if(window.prompt("What Do you see?") === "picture 1"){
+        window.alert("Wow You won!");
       }else{
-        alert("You fail!");
+        alert("Try Again!");
       }
     }
     //For Picture 2
     if(clickedCoords(pageX,pageY,[242,94,257,205])){
-      if(prompt("What Do you see?") === "picture 2"){
-        alert("Wow You won!");
+      if(window.prompt("What Do you see?") === "picture 2"){
+        window.alert("Wow You won!");
       }else{
-        alert("You fail!");
+        alert("Try Again!");
       }
     }
     //For Picture 3
     if(clickedCoords(pageX,pageY,[361,87,436,225])){
-      if(prompt("What Do you see?") === "picture 3"){
-        alert("Wow You won!");
+      if(window.prompt("What Do you see?") === "picture 3"){
+        window.alert("Wow You won!");
       }else{
-        alert("You fail!");
+        alert("Try Again!");
       }
     }
   }
