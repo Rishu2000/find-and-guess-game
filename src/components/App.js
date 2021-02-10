0c419ddf89180125a1d9f8c5cf233144b2f30362
Your Name <you@example.com>
2021-02-10 11:08:35 -0600

    Added clickEvent handler.

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,15 @@ import "../styles/App.css";
 import React, { Component } from 'react'
 
 export default class App extends Component {
+  handleClick = (e) => {
+    e.preventDefault();
+    console.log("object");
+    //console.log(Object.keys(e));
+  }
   render() {
     return (
       <div className="App">
-        <div className="clickArea">
+        <div className="clickArea" onClick={this.handleClick}>
 
         </div>
       </div>
